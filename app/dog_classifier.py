c50a3aeb2b3c40757efe07bb9f8d01728f7e0a04
Jesslzy <126081495+Jesslzy@users.noreply.github.com>
2023-05-22 00:26:58 +0100

    displaying image on flask

--- app/dog_classifier.py
+++ app/dog_classifier.py
@@ -7,7 +7,9 @@ from flask import render_template, request
 from werkzeug.utils import secure_filename
 
 from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
 from keras.preprocessing import image
+from keras.models import load_model
 from keras import backend as K 
 from extract_bottleneck_features import *
 
@@ -27,10 +29,9 @@ def path_to_tensor(img_path):
 def face_detector(img_path):
     """
     """
-    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+    face_cascade = cv2.CascadeClassifier('./haarcascades/haarcascade_frontalface_alt.xml')
     img = cv2.imread(img_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray)
+    faces = face_cascade.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
     return len(faces) > 0
 
 
@@ -77,17 +78,17 @@ def predict_image(img_path):
     dog_breed_predicted = Inception_predict_breed(img_path).split(".")[-1]
     
     if dog_detector(img_path):
-        print("The image you provided looks like an %s." % dog_breed_predicted)
+        return("The image you provided looks like an %s." % dog_breed_predicted)
     elif face_detector(img_path):
-        print("The image you provided is a human.")
+        return("The image you provided is a human.")
     else:
-        print("The image you provided is neither a dog nor a human.")
+        return("The image you provided is neither a dog nor a human.")
 
         
         
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './test_images/'
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'test_images')
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
 
 
@@ -99,36 +100,29 @@ def index():
     # render web page with plotly graphs
     return render_template('master_dog.html')
 
-@app.route('/go', methods = ['POST'])
+@app.route('/go', methods = ['GET', 'POST'])
 def go():
-"""
-Partially adapted from https://blog.filestack.com/thoughts-and-knowledge/secure-file-upload-flask-python/
-"""
+    """
+    Partially adapted from https://blog.filestack.com/thoughts-and-knowledge/secure-file-upload-flask-python/
+    """
     # use model to predict classification for query
-    file_uploaded = request.files['file']
-    filename = secure_filename(file_uploaded.filename)
-    print(filename)
-    if filename != '':
-        file_extension = os.path.splitext(filename)[1]
-        if file_extension not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
-        file_uploaded.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        res = predict_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        img = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if request.method == 'POST':
+        file_uploaded = request.files['file']
+        filename = secure_filename(file_uploaded.filename)
+        print(filename)
+        if filename != '':
+            file_extension = os.path.splitext(filename)[1]
+            if file_extension not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            file_uploaded.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            res = predict_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         
     # This will render the go.html Please see that file. 
     return render_template(
         'go_dog.html',
-        result=res
-    )
-
-@app.route('/show_image')
-def displayImage():
-"""
-Adapted from https://thinkinfi.com/upload-and-display-image-in-flask-python/?utm_content=cmp-true
-"""  
-    # Display image in Flask application web page
-    return render_template('show_image.html', img=img)
+        result=res,
+        img=img)
 
 def main():
     app.run(host='0.0.0.0', port=3000, debug=True)
